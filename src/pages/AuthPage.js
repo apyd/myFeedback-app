8049575ba2b1f5f292f53a179513809f3f88ac54
Artur <arturpydych@gmail.com>
2021-08-18 13:35:14 +0200

    Auth page

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,5 +1,35 @@
+import AuthForm from '../components/Forms/AuthForm/AuthForm'
+import authImg from '../assets/auth-image.jpeg'
+import styled from 'styled-components'
+import { breakpoints } from '../utils/breakpoints'
+
+const AuthWrapper = styled.div`
+  display: flex;
+  justify-content: stretch;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
+    rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 90%;
+`
+
+const Img = styled.img`
+  display: none;
+  max-height: 600px;
+  @media ${breakpoints.laptop} {
+    display: block;
+  }
+`
+
 const Auth = () => {
-  return <div>Auth</div>
+  return (
+    <AuthWrapper>
+      <Img src={authImg} alt="Authentication" />
+      <AuthForm />
+    </AuthWrapper>
+  )
 }
 
 export default Auth
