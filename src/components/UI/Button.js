51a63d02a6d881258a308ec62c15680516a75b28
Artur <arturpydych@gmail.com>
2021-08-18 13:37:40 +0200

    Button/Text input components

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,5 +1,80 @@
-const Button = (props) => {
-  return <button>{props.children}</button>
+import styled from 'styled-components'
+
+const StyledButton = styled.button`
+  font-size: 1rem;
+  font-family: inherit;
+  font-weight: bold;
+  padding: 0.8em 2em;
+  border: none;
+  /* border: 1px solid var(--blue); */
+  border-radius: 2px;
+  color: white;
+  background-color: var(--blue);
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+const StyledSSOButton = styled.button`
+  display: flex;
+  position: relative;
+  align-items: stretch;
+  justify-content: stretch;
+  font-size: 1rem;
+  font-family: inherit;
+  padding: 0.8em 1em 0.8em 70px;
+  border: none;
+  border-radius: 2px;
+  background-color: var(--blue);
+  color: white;
+  font-weight: bold;
+  &:hover {
+    cursor: pointer;
+  }
+  &:before {
+    position: absolute;
+    content: '';
+    background: url(${(props) => props.img}) center center no-repeat;
+    width: 50px;
+    background-color: white;
+    height: 100%;
+    left: 0%;
+    top: 0%;
+  }
+`
+
+const Link = styled.button`
+  font-size: 1rem;
+  font-family: inherit;
+  padding: 0;
+  margin: 0;
+  padding-bottom: 2px;
+  border: none;
+  border-bottom: 1px solid var(--light-grey);
+  color: var(--blue);
+  background-color: transparent;
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+const onClickHandler = (e, props) => {
+  e.preventDefault()
+  props.onClick()
 }
 
-export default Button
+export const Button = (props) => {
+  return <StyledButton onClick={props.onClick}>{props.children}</StyledButton>
+}
+
+export const SSOButton = (props) => {
+  return <StyledSSOButton img={props.img}>{props.children}</StyledSSOButton>
+}
+
+export const ButtonLink = (props) => {
+  return (
+    <Link onClick={(event) => onClickHandler(event, props)}>
+      {props.children}
+    </Link>
+  )
+}
--- src/components/UI/TextInput.js
+++ src/components/UI/TextInput.js
@@ -1,9 +1,31 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+import { Input, Label } from '../Forms/Forms'
+
+const InputWrapper = styled.div`
+  display: flex;
+  position: relative;
+  width: 100%;
+  margin: 20px auto;
+`
+
 const TextInput = (props) => {
+  const [isTouched, setIsTouched] = useState(false)
+
   return (
-    <>
-      <input></input>
-      <label></label>
-    </>
+    <InputWrapper>
+      <Input
+        isDirty={isTouched}
+        onBlur={() => setIsTouched(true)}
+        id={props.id}
+        name={props.name}
+        placeholder={props.placeholder}
+        type={props.type}
+      />
+      <Label isDirty={isTouched} htmlFor={props.id}>
+        {props.label}
+      </Label>
+    </InputWrapper>
   )
 }
 
