e5da88e314927586b164ff28bfb211a57e50979f
Artur <arturpydych@gmail.com>
2021-08-18 13:35:42 +0200

    Utils - breakpoints

---
+++ src/utils/breakpoints.js
@@ -0,0 +1,11 @@
+const maxSize = {
+  mobile: '480px',
+  tablet: '768px',
+  laptop: '1279px',
+}
+
+export const breakpoints = {
+  tablet: `(min-width: ${maxSize.mobile})`,
+  laptop: `(min-width: ${maxSize.tablet})`,
+  desktop: `(min-width: ${maxSize.laptop + 1})`,
+}
