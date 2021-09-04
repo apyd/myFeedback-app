13a23d468898ee9f4cf98d0a0a2614e0613fdd89
Artur <arturpydych@gmail.com>
2021-09-04 10:26:35 +0200

    Default black color for text styled components

--- src/components/UI/Text.js
+++ src/components/UI/Text.js
@@ -3,21 +3,25 @@ import styled from 'styled-components'
 const StyledHeading1 = styled.h1`
   font-size: 1.3rem;
   margin: 10px 0;
+  color: ${(props) => props.color || 'black'};
 `
 const StyledHeading2 = styled.h2`
   font-size: 1.15rem;
   margin: 10px 0;
+  color: ${(props) => props.color || 'black'};
 `
 const StyledHeading3 = styled.h3`
   font-size: 1.2rem;
   margin: 0;
   padding: 0;
+  color: ${(props) => props.color || 'black'};
 `
 
 const StyledParagraph = styled.p`
   font-size: 1rem;
   margin: 20px 5px;
   padding: 0;
+  color: ${(props) => props.color || 'black'};
 `
 
 const StyledSpan = styled.span`
@@ -25,19 +29,19 @@ const StyledSpan = styled.span`
 `
 
 export const Heading1 = (props) => {
-  return <StyledHeading1>{props.children}</StyledHeading1>
+  return <StyledHeading1 color={props.color}>{props.children}</StyledHeading1>
 }
 
 export const Heading2 = (props) => {
-  return <StyledHeading2>{props.children}</StyledHeading2>
+  return <StyledHeading2 color={props.color}>{props.children}</StyledHeading2>
 }
 
 export const Heading3 = (props) => {
-  return <StyledHeading3>{props.children}</StyledHeading3>
+  return <StyledHeading3 color={props.color}>{props.children}</StyledHeading3>
 }
 
 export const Paragraph = (props) => {
-  return <StyledParagraph>{props.children}</StyledParagraph>
+  return <StyledParagraph color={props.color}>{props.children}</StyledParagraph>
 }
 
 export const Span = (props) => {
