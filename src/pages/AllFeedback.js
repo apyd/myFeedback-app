a6ab793cf18a4ee392d778eb98c7676180772fe2
Artur <arturpydych@gmail.com>
2021-09-09 22:18:18 +0200

    Product feedback page

--- src/pages/AllFeedback.js
+++ src/pages/AllFeedback.js
@@ -1,5 +1,28 @@
+import styled from 'styled-components'
+import { Button } from '../components/UI/Button'
+
+const ActionsWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  position: relative;
+  top: -30px;
+  left: -20px;
+  width: 100vw;
+  padding: 10px 20px;
+  background-color: var(--blue);
+  color: white;
+  font-weight: bold;
+`
+
 const Feedbacks = () => {
-  return <p>Feedbacks</p>
+  return (
+    <>
+      <ActionsWrapper>
+        <p>6 suggestions</p>
+        <Button>+ Add feedback</Button>
+      </ActionsWrapper>
+    </>
+  )
 }
 
 export default Feedbacks
