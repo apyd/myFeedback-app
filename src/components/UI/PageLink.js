afbf3c31cbdcf2934fd8f23e32ac6d2c3dbc36a0
Artur <arturpydych@gmail.com>
2021-09-10 17:52:38 +0200

    PageLink component

--- src/components/UI/PageLink.js
+++ src/components/UI/PageLink.js
@@ -1,7 +1,36 @@
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
-const PageLink = (props) => {
-  return <Link to={props.to}>{props.children}</Link>
+const StyledLink = styled(Link)`
+  padding: 0;
+  margin: 0 5px;
+  padding-bottom: 2px;
+  border: none;
+  border-bottom: 1px solid var(--light-grey);
+  font-size: 1rem;
+  font-family: inherit;
+  text-decoration: none;
+  color: var(--blue);
+  background-color: transparent;
+  &:hover {
+    cursor: pointer;
+    border-bottom: 1px solid var(--grey);
+  }
+`
+
+const PageLink = ({ to, children }) => {
+  return <StyledLink to={to}>{children}</StyledLink>
+}
+
+PageLink.propTypes = {
+  to: PropTypes.string,
+  children: PropTypes.node,
+}
+
+PageLink.defaultProps = {
+  to: '/',
+  children: null,
 }
 
 export default PageLink
