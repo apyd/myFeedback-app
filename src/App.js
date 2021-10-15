bf92a8f3d3485dd8a0942a2a77b0f5b023915e0b
Artur <arturpydych@gmail.com>
2021-10-15 07:29:15 +0200

    Theme provider in app component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './theme/globalStyles'
+import useDarkMode from './hooks/useDarkMode'
+import { lightTheme, darkTheme } from './theme/theme'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { AppRoutes } from './routing/AppRoutes'
 import Home from './pages/Home'
@@ -8,49 +12,56 @@ import Roadmap from './pages/Roadmap'
 import Preferences from './pages/Preferences'
 import NotFound from './pages/NotFound'
 
-import './App.css'
 import Layout from './layout/Layout'
 import Auth from './pages/Auth'
 
 function App() {
-  console.log(AppRoutes.PRODUCTS)
+  const [theme, toggleTheme, componentMounted] = useDarkMode()
+  let themeMode = theme === 'light' ? lightTheme : darkTheme
+
+  if (!componentMounted) {
+    return <div />
+  }
 
   return (
     <Router>
-      <Layout>
-        <Switch>
-          <Route path={AppRoutes.HOME} exact>
-            <Home />
-          </Route>
-          <Route path={AppRoutes.LOGIN} exact>
-            <Auth />
-          </Route>
-          <Route path={AppRoutes.REGISTER} exact>
-            <Auth />
-          </Route>
-          <Route path={AppRoutes.FORGOT_PASSWORD} exact>
-            <Auth />
-          </Route>
-          <Route path={AppRoutes.PRODUCTS}>
-            <Products />
-          </Route>
-          <Route path={AppRoutes.FEEDBACK} exact>
-            <AllFeedback />
-          </Route>
-          <Route path={`${AppRoutes.FEEDBACK_DETAILS}/:id`}>
-            <FeedbackDetails />
-          </Route>
-          <Route path={AppRoutes.ROADMAP}>
-            <Roadmap />
-          </Route>
-          <Route path={AppRoutes.PREFERENCES}>
-            <Preferences />
-          </Route>
-          <Route path={'*'}>
-            <NotFound />
-          </Route>
-        </Switch>
-      </Layout>
+      <ThemeProvider theme={themeMode}>
+        <GlobalStyles />
+        <Layout activeTheme={theme} toggleTheme={toggleTheme}>
+          <Switch>
+            <Route path={AppRoutes.HOME} exact>
+              <Home />
+            </Route>
+            <Route path={AppRoutes.LOGIN} exact>
+              <Auth />
+            </Route>
+            <Route path={AppRoutes.REGISTER} exact>
+              <Auth />
+            </Route>
+            <Route path={AppRoutes.FORGOT_PASSWORD} exact>
+              <Auth />
+            </Route>
+            <Route path={AppRoutes.PRODUCTS}>
+              <Products />
+            </Route>
+            <Route path={AppRoutes.FEEDBACK} exact>
+              <AllFeedback />
+            </Route>
+            <Route path={`${AppRoutes.FEEDBACK_DETAILS}/:id`}>
+              <FeedbackDetails />
+            </Route>
+            <Route path={AppRoutes.ROADMAP}>
+              <Roadmap />
+            </Route>
+            <Route path={AppRoutes.PREFERENCES}>
+              <Preferences />
+            </Route>
+            <Route path={AppRoutes.NOTFOUND}>
+              <NotFound />
+            </Route>
+          </Switch>
+        </Layout>
+      </ThemeProvider>
     </Router>
   )
 }
