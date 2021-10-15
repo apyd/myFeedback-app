99bac6d0e3768036ac7a2d1313b3aefdb2a3bb1c
Artur <arturpydych@gmail.com>
2021-10-15 07:27:30 +0200

    Theme toggle

---
+++ src/components/ThemeToggle/ThemeToggle.js
@@ -0,0 +1,19 @@
+import Button from '../UI/Button'
+import { ReactComponent as HalfMoon } from '../../assets/halfmoon.svg'
+import { ReactComponent as Sun } from '../../assets/sun.svg'
+
+const ThemeToggle = ({ activeTheme, toggleTheme }) => {
+  return (
+    <Button
+      margin="0px 0px 0px 15px"
+      padding="0"
+      variant="ghost"
+      onClick={toggleTheme}
+    >
+      {activeTheme === 'dark' && <HalfMoon width="32px" height="32px" />}
+      {activeTheme === 'light' && <Sun width="32px" height="32px" />}
+    </Button>
+  )
+}
+
+export default ThemeToggle
